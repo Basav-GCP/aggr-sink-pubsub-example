2adb1c13afb992e51e5f3a3e873c6b06fa011804
Basav-GCP <62184694+Basav-GCP@users.noreply.github.com>
2022-05-31 18:56:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,19 @@
+terraform {
+  required_version = ">=0.12.0"
+}
 module "pubsub" {
-  source  = "git::https://github.com/Basav-GCP/gbdi-dbai-pubsub.git?ref=0.1"
-  version = "~> 1.8"
+  source  = "git::https://github.com/Basav-GCP/gbdi.git?ref=0.3.8"
   topic      = "postgres-log-sink"
   project_id = "vaulted-keel-333115"
-  kms_key_name = "key"
+  service_account = "msscloud-sa@vaulted-keel-333115.iam.gserviceaccount.com"
+  env = var.env
+  region=var.region
   pull_subscriptions = [
     {
-      name                    = "pull"                                               // required
+      name                    = "postgres-pull"                                      // required
       ack_deadline_seconds    = 20                                                   // optional
       dead_letter_topic       = "projects/my-pubsub-project/topics/example-dl-topic" // optional
       max_delivery_attempts   = 5                                                    // optional
-      service_account         = "msscloud-sa@vaulted-keel-333115.iam.gserviceaccount.com" // required          // optional
-    }
+     }
   ]
 }
